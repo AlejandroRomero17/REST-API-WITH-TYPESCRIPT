dc5b2945739d982a8228878e5c60ae301b4b8b59
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-08-03 08:43:46 -0500

    update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export default {
   port: 3001,
-  dbUri: "mongodb+srv://AlejandroRomero17:AlejandroRomero17@cluster0.mongodb.net/db_rest_api_ts?retryWrites=true&w=majority",
+  dbUri: "mongodb+srv://AlejandroRomero17:AlejandroRomero17@alejandroromero.elzpk6w.mongodb.net/"
 }
